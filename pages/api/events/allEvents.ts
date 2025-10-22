32a0d206dd1600cf81c1985f06e9817e9847bca6
MattsVh <101123657+MattsVh@users.noreply.github.com>
2025-10-22 09:28:37 +0200

    Change in caching

--- pages/api/events/allEvents.ts
+++ pages/api/events/allEvents.ts
@@ -1,8 +1,4 @@
-import { url } from "inspector"
 import type { NextApiRequest, NextApiResponse } from "next"
-import NodeCache from "node-cache"
-
-const cache = new NodeCache({ stdTTL: 600 }) // Cache TTL set to 10 minutes
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const PAGE_ID = process.env.FACEBOOK_EVENTS_ID
@@ -10,13 +6,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const urlAllEvents = `https://graph.facebook.com/v22.0/${PAGE_ID}/events?fields=id,name,description,start_time,cover&access_token=${ACCESS_TOKEN}`
   
+  // Set caching headers
+  res.setHeader('Cache-Control', 'public, s-maxage=600, stale-while-revalidate=86400')
 
   try {
-    // Check if events are cached
-    const cachedEvents = cache.get("events")
-    if (cachedEvents) {
-      return res.status(200).json(cachedEvents)
-    }
 
     const response = await fetch(urlAllEvents)
     const data = await response.json()
@@ -34,9 +27,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       image: event.cover?.source || "/images/default-event.jpg",
       description: event.description || "Geen beschrijving beschikbaar",
     }))
-
-    // Cache the events
-    cache.set("events", events)
 
     res.status(200).json(events)
   } catch (error) {
